d43597eb950728c0e8302d5e5efec91a1523605f
manvillarim <manvillarim@gmail.com>
2025-10-14 18:22:46 -0300

    case study updates

--- case-study/conf/.certora_internal/25_10_06_17_31_18_732/.certora_sources/.pre_autofinders.0/conf/Collector/CollectorOptimized.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier:  BUSL-1.1
-pragma solidity ^0.8.0;
-
-import {Collector} from "../aave-v3-origin-liquidation-gas-fixes/src/contracts/treasury/Collector.sol";
-
-contract CollectorOptimized is Collector {
-
-}
